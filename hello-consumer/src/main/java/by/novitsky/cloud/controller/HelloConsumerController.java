cd30e0256062a26378dd2bc5480702ba3b5b60f3
AntonNovitsky <7075167@gmail.com>
2019-09-25 12:38:02 +0300

    First test implementation of hystrix

--- hello-consumer/src/main/java/by/novitsky/cloud/controller/HelloConsumerController.java
+++ hello-consumer/src/main/java/by/novitsky/cloud/controller/HelloConsumerController.java
@@ -1,18 +1,21 @@
 package by.novitsky.cloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloConsumerController {
 
-    private static final String PRODUCER_SERVICE_NAME = "HELLO-SERVER";
+    private static final String PRODUCER_SERVICE_NAME = "hello-server";
     private static final String PRODUCER_PATH = "/hello";
+    private static final String PRODUCER_UNSAFE_PATH = "/failing?failurePercent=";
+    private static final String PRODUCER_LATENCY_PATH = "/latency?latency=";
 
     private RestTemplate restTemplate;
     private DiscoveryClient discoveryClient;
@@ -25,14 +28,31 @@ public class HelloConsumerController {
 
     @GetMapping("/hello")
     public String getHello(){
-        ServiceInstance serviceInstance = discoveryClient.getInstances(PRODUCER_SERVICE_NAME).get(0);
-
-        String url =serviceInstance.getUri().toString();
-        url += PRODUCER_PATH;
+        String url = getUrl(PRODUCER_PATH);
         String requestResult = new RestTemplate().getForObject(url, String.class);
         requestResult = "Got data from producer: " + requestResult;
 
         return requestResult;
+    }
+
+    @GetMapping("/failing")
+    @HystrixCommand(fallbackMethod = "hystrixFallback")
+    public String getUnsafe(@RequestParam(defaultValue = "50") Integer percent){
+        String url = getUrl(PRODUCER_UNSAFE_PATH + percent);
+        String requestResult = new RestTemplate().getForObject(url, String.class);
+        requestResult = "From producer: " + requestResult;
+        return requestResult;
+    }
+
+    private String hystrixFallback(Integer input){
+        return "Error accessing " + PRODUCER_SERVICE_NAME + " with failing percent " + input;
+    }
+
+    private String getUrl(String path){
+        ServiceInstance serviceInstance = discoveryClient.getInstances(PRODUCER_SERVICE_NAME).get(0);
+        String url =serviceInstance.getUri().toString();
+        url += path;
+        return url;
     }
 
 }
--- hello-server/src/main/java/by/novitsky/cloud/controller/HelloServerController.java
+++ hello-server/src/main/java/by/novitsky/cloud/controller/HelloServerController.java
@@ -1,9 +1,11 @@
 package by.novitsky.cloud.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 @RestController
 public class HelloServerController {
@@ -12,4 +14,14 @@ public class HelloServerController {
     public LocalDateTime hello(){
         return LocalDateTime.now();
     }
+
+    @GetMapping("/failing")
+    public String failingRequest(@RequestParam(defaultValue = "50") Integer failurePercent){
+        Integer isFailed = (int) (Math.random() * 100) + 1;
+        if (failurePercent > isFailed){
+            throw new RuntimeException("Failed, rnd = " + isFailed);
+        }
+        return "Not failed, rnd = " + isFailed;
+    }
+
 }
