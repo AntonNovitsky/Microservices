2c7650314b5d9273356ad9aaf45a6b18694bcb2f
AntonNovitsky <7075167@gmail.com>
2019-09-26 12:09:02 +0300

    First implementation of ribbon

--- hello-consumer/src/main/java/by/novitsky/cloud/controller/HelloConsumerController.java
+++ hello-consumer/src/main/java/by/novitsky/cloud/controller/HelloConsumerController.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +16,16 @@ public class HelloConsumerController {
     private static final String PRODUCER_SERVICE_NAME = "hello-server";
     private static final String PRODUCER_PATH = "/hello";
     private static final String PRODUCER_UNSAFE_PATH = "/failing?failurePercent=";
-    private static final String PRODUCER_LATENCY_PATH = "/latency?latency=";
 
     private RestTemplate restTemplate;
     private DiscoveryClient discoveryClient;
+    private LoadBalancerClient loadBalancerClient;
 
     @Autowired
-    public HelloConsumerController(RestTemplate restTemplate, DiscoveryClient discoveryClient) {
+    public HelloConsumerController(RestTemplate restTemplate, DiscoveryClient discoveryClient, LoadBalancerClient loadBalancerClient) {
         this.restTemplate = restTemplate;
         this.discoveryClient = discoveryClient;
+        this.loadBalancerClient = loadBalancerClient;
     }
 
     @GetMapping("/hello")
@@ -32,6 +34,14 @@ public class HelloConsumerController {
         String requestResult = new RestTemplate().getForObject(url, String.class);
         requestResult = "Got data from producer: " + requestResult;
 
+        return requestResult;
+    }
+
+    @GetMapping("/balancedhello")
+    public String getBalancedHello(){
+        String url = getBalancedUrl(PRODUCER_PATH);
+        String requestResult = new RestTemplate().getForObject(url, String.class);
+        requestResult = "Response from " + url + " : " + requestResult;
         return requestResult;
     }
 
@@ -50,8 +60,15 @@ public class HelloConsumerController {
 
     private String getUrl(String path){
         ServiceInstance serviceInstance = discoveryClient.getInstances(PRODUCER_SERVICE_NAME).get(0);
-        String url =serviceInstance.getUri().toString();
+        String url = serviceInstance.getUri().toString();
         url += path;
+        return url;
+    }
+
+    private String getBalancedUrl(String path){
+        ServiceInstance serviceInstance = loadBalancerClient.choose(PRODUCER_SERVICE_NAME);
+        String url = serviceInstance.getUri().toString();
+        url = url + path;
         return url;
     }
 
