0742eaf61f41e2472084fa85e0de34b03db069e4
AntonNovitsky <7075167@gmail.com>
2019-09-27 12:28:28 +0300

    First implementation of feign client

--- hello-consumer/src/main/java/by/novitsky/cloud/controller/HelloConsumerController.java
+++ hello-consumer/src/main/java/by/novitsky/cloud/controller/HelloConsumerController.java
@@ -1,14 +1,19 @@
 package by.novitsky.cloud.controller;
 
+import by.novitsky.cloud.entity.Item;
+import by.novitsky.cloud.service.FeignPlaceholderService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class HelloConsumerController {
@@ -20,12 +25,13 @@ public class HelloConsumerController {
     private RestTemplate restTemplate;
     private DiscoveryClient discoveryClient;
     private LoadBalancerClient loadBalancerClient;
+    private FeignPlaceholderService feignPlaceholderController;
 
-    @Autowired
-    public HelloConsumerController(RestTemplate restTemplate, DiscoveryClient discoveryClient, LoadBalancerClient loadBalancerClient) {
+    public HelloConsumerController(RestTemplate restTemplate, DiscoveryClient discoveryClient, LoadBalancerClient loadBalancerClient, FeignPlaceholderService feignPlaceholderController) {
         this.restTemplate = restTemplate;
         this.discoveryClient = discoveryClient;
         this.loadBalancerClient = loadBalancerClient;
+        this.feignPlaceholderController = feignPlaceholderController;
     }
 
     @GetMapping("/hello")
@@ -54,6 +60,16 @@ public class HelloConsumerController {
         return requestResult;
     }
 
+    @GetMapping(value = "/posts", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Item> getPosts(){
+        return feignPlaceholderController.getAllItems();
+    }
+
+    @GetMapping(value = "/posts/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Item getItem(@PathVariable("id") Long id){
+        return feignPlaceholderController.getItem(id);
+    }
+
     private String hystrixFallback(Integer input){
         return "Error accessing " + PRODUCER_SERVICE_NAME + " with failing percent " + input;
     }
@@ -71,6 +87,7 @@ public class HelloConsumerController {
         url = url + path;
         return url;
     }
+
 
 
 
